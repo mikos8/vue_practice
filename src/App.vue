ae15af78d5f2818f5d7784d1e4b5d54c15ba45d7
mikos8 <mikos.zh8@gmail.com>
2021-07-29 19:59:49 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>caount like {{ likes }}</div>
+  <div>caount dislike{{ dislike }}</div>
+  <div>
+    <button v-on:click="addLike">like</button>
+    <button v-on:click="addDislike">dislike</button>
   </div>
-  <router-view/>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislike: 0,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislike += 1;
+    },
+  },
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
